aabbbec731886b31d05e6461ed8dcee1c6bf87dd
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2024-03-20 10:25:22 +1100

    fix, minor changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SIT725 prac3
+
+To run the example, please run following commands
+
+```
+npm install
+npm start
+```
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-app = express();
+const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
